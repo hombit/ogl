101341316c8a79926ea730ec4cb2ccc2ffd2fb54
Konstantin Malanchev <hombit@gmail.com>
2017-02-16 17:30:31 +0300

    Tutorial 2

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -8,6 +8,8 @@ GLFWwindow* window;
 #include <glm/glm.hpp>
 using namespace glm;
 
+#include <common/shader.hpp>
+
 int playground()
 {
 	// Initialise GLFW
@@ -36,6 +38,7 @@ int playground()
 	glfwMakeContextCurrent(window);
 
 	// Initialize GLEW
+    glewExperimental = true; // Needed for core profile
 	if (glewInit() != GLEW_OK) {
 		fprintf(stderr, "Failed to initialize GLEW\n");
 		getchar();
@@ -46,20 +49,62 @@ int playground()
 	// Ensure we can capture the escape key being pressed below
 	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
+	// Blue background
+	glClearColor(0.2f, 0.3f, 0.4f, 0.0f);
 
-	// White background
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    // Create Vertex Array Object (VAO)
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    // Triangle
+    static const GLfloat g_vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f,  1.0f, 0.0f,
+    };
+
+    // This will identify our vertex buffer
+    GLuint vertexbuffer;
+    // Generate 1 buffer, put the resulting identifier in vertexbuffer
+    glGenBuffers(1, &vertexbuffer);
+    // The following commands will talk about our 'vertexbuffer' buffer
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    // Give our vertices to OpenGL.
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+    GLuint programID = LoadShaders(
+            "playground/SimpleVertexShader.glsl",
+            "playground/SimpleFragmentShader.glsl"
+    );
 
 	do{
-		glClear(GL_COLOR_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Draw nothing, see you in tutorial 2 !
+		// Use our shader
+		glUseProgram(programID);
+
+        // 1rst attribute buffer : vertices
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+                0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+                3,                  // size
+                GL_FLOAT,           // type
+                GL_FALSE,           // normalized?
+                0,                  // stride
+                (void*)0            // array buffer offset
+        );
+
+        // Draw the triangle !
+        glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+        glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 
-	} // Check if the ESC key was pressed or the window was closed
+	} // Check if the Space key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_SPACE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
