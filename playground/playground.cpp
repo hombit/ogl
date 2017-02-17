7a62de299dff882e5fa703498340beffbf96f4df
Konstantin Malanchev <hombit@gmail.com>
2017-02-17 18:22:55 +0300

    Return to clean Tutorial 4

--- playground/playground.cpp
+++ playground/playground.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <vector>
 
 #include <GL/glew.h>
 
@@ -8,22 +9,10 @@ GLFWwindow* window;
 
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 using namespace glm;
 
 #include <common/shader.hpp>
-
-
-void CubeColor(int x, GLfloat g_color_buffer_data[]){
-	x = x / 24 % 10;
-	for ( int i = 0; i < 6; ++i ) {
-		for (int j = 0; j < 6; ++j) {
-			g_color_buffer_data[i * 18 + j * 3 + 0] = (float) x / 10;
-			g_color_buffer_data[i * 18 + j * 3 + 1] = (float) (i + 1) / 6.0f;
-			g_color_buffer_data[i * 18 + j * 3 + 2] = 0.0f;
-		}
-	}
-}
-
 
 int playground()
 {
@@ -127,34 +116,49 @@ int playground()
     // Give our vertices to OpenGL.
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	GLfloat g_color_buffer_data[12*3*3];
-	CubeColor(0, g_color_buffer_data);
+	static const GLfloat g_color_buffer_data[] = {
+			0.583f,  0.771f,  0.014f,
+			0.609f,  0.115f,  0.436f,
+			0.327f,  0.483f,  0.844f,
+			0.822f,  0.569f,  0.201f,
+			0.435f,  0.602f,  0.223f,
+			0.310f,  0.747f,  0.185f,
+			0.597f,  0.770f,  0.761f,
+			0.559f,  0.436f,  0.730f,
+			0.359f,  0.583f,  0.152f,
+			0.483f,  0.596f,  0.789f,
+			0.559f,  0.861f,  0.639f,
+			0.195f,  0.548f,  0.859f,
+			0.014f,  0.184f,  0.576f,
+			0.771f,  0.328f,  0.970f,
+			0.406f,  0.615f,  0.116f,
+			0.676f,  0.977f,  0.133f,
+			0.971f,  0.572f,  0.833f,
+			0.140f,  0.616f,  0.489f,
+			0.997f,  0.513f,  0.064f,
+			0.945f,  0.719f,  0.592f,
+			0.543f,  0.021f,  0.978f,
+			0.279f,  0.317f,  0.505f,
+			0.167f,  0.620f,  0.077f,
+			0.347f,  0.857f,  0.137f,
+			0.055f,  0.953f,  0.042f,
+			0.714f,  0.505f,  0.345f,
+			0.783f,  0.290f,  0.734f,
+			0.722f,  0.645f,  0.174f,
+			0.302f,  0.455f,  0.848f,
+			0.225f,  0.587f,  0.040f,
+			0.517f,  0.713f,  0.338f,
+			0.053f,  0.959f,  0.120f,
+			0.393f,  0.621f,  0.362f,
+			0.673f,  0.211f,  0.457f,
+			0.820f,  0.883f,  0.371f,
+			0.982f,  0.099f,  0.879f
+	};
 
 	GLuint color_buffer;
 	glGenBuffers(1, &color_buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_DYNAMIC_DRAW);
-
-	static const GLfloat g_triangle_buffer_data[] = {
-			-1.0f, -1.0f, 0.0f,
-			0.0f,  1.0f, 0.0f,
-			1.0f, -1.0f, 0.0f,
-	};
-	GLuint triangle_buffer;
-	glGenBuffers(1, &triangle_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, triangle_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_triangle_buffer_data), g_triangle_buffer_data, GL_STATIC_DRAW);
-
-	static const GLfloat g_color_triangle_buffer_data[] = {
-			0.0f, 0.0f, 0.0f,
-			0.0f, 0.0f, 0.0f,
-			0.0f, 0.0f, 0.0f,
-	};
-
-	GLuint color_triangle_buffer;
-	glGenBuffers(1, &color_triangle_buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, color_triangle_buffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_triangle_buffer_data), g_color_triangle_buffer_data, GL_DYNAMIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
     GLuint programID = LoadShaders(
             "playground/TransformVertexShader.glsl",
@@ -166,23 +170,20 @@ int playground()
 //	glm::mat4 Projection = glm::ortho(-5.0f,5.0f,-5.0f,5.0f,0.0f,50.0f);
 	// Camera matrix
 	glm::mat4 View = glm::lookAt(
-			glm::vec3(0,0,4), // Camera is at (4,3,3), in World Space
+			glm::vec3(4,3,-3), // Camera is at (4,3,3), in World Space
 			glm::vec3(0,0,0), // and looks at the origin
 			glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
 	);
 	// Model matrix : an identity matrix (model will be at the origin)
-	glm::mat4 TranslationMatrix = glm::translate(glm::vec3(2,0,0));
-    glm::mat4 RotationMatrix = glm::rotate(45.0f, glm::vec3(1,1,1));
+	glm::mat4 TranslationMatrix = glm::translate(glm::vec3(0,0,0));
+    glm::mat4 RotationMatrix = glm::rotate(0.0f, glm::vec3(0,0,1));
 	glm::mat4 ScaleMatrix = glm::scale(glm::vec3(1,1,1));
 	glm::mat4 Model = TranslationMatrix * RotationMatrix * ScaleMatrix;
 	// Our ModelViewProjection : multiplication of our 3 matrices
-	glm::mat4 mvp_cube = Projection * View * Model; // Remember, matrix multiplication is the other way around
+	glm::mat4 mvp = Projection * View * Model; // Remember, matrix multiplication is the other way around
 	// Get a handle for our "MVP" uniform
 	// Only during the initialisation
 	GLint MatrixID = glGetUniformLocation(programID, "MVP");
-	glm::mat4 mvp_triangle = Projection * View * glm::translate(glm::vec3(-2,0,0));
-
-	int x = 0;
 
 	do{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -190,14 +191,9 @@ int playground()
 		// Use our shader
 		glUseProgram(programID);
 
-		glBindBuffer(GL_ARRAY_BUFFER, color_buffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_DYNAMIC_DRAW);
-		CubeColor(x++, g_color_buffer_data);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_DYNAMIC_DRAW);
-
 		// Send our transformation to the currently bound shader, in the "MVP" uniform
 		// This is done in the main loop since each model will have a different MVP matrix (At least for the M part)
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp_cube[0][0]);
+		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp[0][0]);
 
         // 1rst attribute buffer : vertices
         glEnableVertexAttribArray(0);
@@ -222,40 +218,9 @@ int playground()
 				(void*)0                          // array buffer offset
 		);
 
+        // Draw the triangle !
         glDrawArrays(GL_TRIANGLES, 0, sizeof(g_vertex_buffer_data)); // Starting from vertex 0; 3 vertices total -> 1 triangle
         glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
-
-		glBindBuffer(GL_ARRAY_BUFFER, color_triangle_buffer);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_triangle_buffer_data), g_color_triangle_buffer_data, GL_DYNAMIC_DRAW);
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &mvp_triangle[0][0]);
-
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, triangle_buffer);
-		glVertexAttribPointer(
-				0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-				3,                  // size
-				GL_FLOAT,           // type
-				GL_FALSE,           // normalized?
-				0,                  // stride
-				(void*)0            // array buffer offset
-		);
-
-		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, color_triangle_buffer);
-		glVertexAttribPointer(
-				1,                                // attribute. No particular reason for 1, but must match the layout in the shader.
-				3,                                // size
-				GL_FLOAT,                         // type
-				GL_FALSE,                         // normalized?
-				0,                                // stride
-				(void*)0                          // array buffer offset
-		);
-
-		glDrawArrays(GL_TRIANGLES, 0, sizeof(g_triangle_buffer_data)); // Starting from vertex 0; 3 vertices total -> 1 triangle
-		glDisableVertexAttribArray(0);
-		glDisableVertexAttribArray(1);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
